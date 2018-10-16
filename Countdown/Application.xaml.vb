fff19ee3c87b182548231a521f78c436ef077580
Ralf Ren <me@ralf.ren>
2018-10-16 22:23:38 +0800

    Basic UI Elements

--- Countdown/Application.xaml.vb
+++ Countdown/Application.xaml.vb
@@ -2,6 +2,24 @@
 
 
     Private Sub Application_Startup(sender As Object, e As StartupEventArgs)
+
+        ' check resources & directories
+        If Not IO.Directory.Exists(GetApplicationDir() & "\Config") Then
+            Try
+                IO.Directory.CreateDirectory(GetApplicationDir() & "\Config")
+            Catch ex As Exception
+                MessageBox.Show("无法创建程序所需目录。程序将退出。", "",
+                                MessageBoxButton.OK, MessageBoxImage.Error)
+                End
+            End Try
+        End If
+
+        If Not IO.File.Exists(GetApplicationDir() & "\Resources\timer_tray.ico") Then
+            MessageBox.Show("找不到程序所需资源，程序将退出。", "",
+                            MessageBoxButton.OK, MessageBoxImage.Error)
+            End
+        End If
+
         If Not IO.File.Exists(GetConfigFile) Then
             Dim wndConfig As New ConfigWindow
             wndConfig.Show()
--- Countdown/Modules/TrayIcon.vb
+++ Countdown/Modules/TrayIcon.vb
@@ -3,25 +3,25 @@
 Module TrayIcon
 
     Dim ico As NotifyIcon
+    Dim wndConfig As ConfigWindow
 
     Public Sub ShowTrayIcon()
         If ico IsNot Nothing Then Exit Sub
 
         ico = New NotifyIcon With {
             .Text = "Aronnax",
-            .Icon = New System.Drawing.Icon(GetApplicationDir() & "Resources\package_tray.ico")
+            .Icon = New System.Drawing.Icon(GetApplicationDir() & "Resources\timer_tray.ico")
         }
 
         Dim menu As New ContextMenu
-
-
-        menu.MenuItems.Add("-")
 
         Dim mnuShowMainWindow As New MenuItem With {
             .Text = "偏好设置 (&P)..."
         }
         AddHandler mnuShowMainWindow.Click, AddressOf ShowMainWindowHandler
         menu.MenuItems.Add(mnuShowMainWindow)
+
+        menu.MenuItems.Add("-")
 
         Dim mnuExit As New MenuItem With {
             .Text = "退出 (&X)..."
@@ -35,11 +35,12 @@ Module TrayIcon
     End Sub
 
     Private Sub ExitHandler(sender As Object, e As EventArgs)
-
+        End
     End Sub
 
     Private Sub ShowMainWindowHandler(sender As Object, e As EventArgs)
-
+        wndConfig = New ConfigWindow
+        wndConfig.Show()
     End Sub
 
 End Module
