36e91e1b32c5018b272d5d5dff8b17820975b9d8
Ralf Ren <me@ralf.ren>
2018-10-16 15:48:27 +0800

    添加项目文件。

---
+++ Countdown/Application.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class Application
+    Private Sub Application_Startup(sender As Object, e As StartupEventArgs)
+
+    End Sub
+
+    ' 应用程序级事件(例如 Startup、Exit 和 DispatcherUnhandledException)
+    ' 可以在此文件中进行处理。
+
+End Class
