c08b60a9618f57e89cc47485aed4b8be7f517e27
Ralf Ren <me@ralf.ren>
2018-10-22 10:29:14 +0800

    Settings UI done.

--- Countdown/ConfigWindow.xaml.vb
+++ Countdown/ConfigWindow.xaml.vb
@@ -1,3 +1,37 @@
 ﻿Public Class ConfigWindow
 
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        lstMenu.SelectedIndex = 0
+        CheckRapidStartCompatibility()
+    End Sub
+
+    Private Sub lstMenu_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
+        Dim item As ListBoxItem = lstMenu.SelectedItem
+        Select Case item.Tag
+            Case "optSchedule"
+                grdOptions.Visibility = Visibility.Hidden
+                grdSchedule.Visibility = Visibility.Visible
+            Case "optOptions"
+                grdSchedule.Visibility = Visibility.Hidden
+                grdOptions.Visibility = Visibility.Visible
+        End Select
+    End Sub
+
+    Private Sub radLast_Checked(sender As Object, e As RoutedEventArgs)
+        txtSchedule.IsEnabled = False
+        txtLast.IsEnabled = True
+    End Sub
+
+    Private Sub radLast_Unchecked(sender As Object, e As RoutedEventArgs)
+        txtLast.IsEnabled = False
+        txtSchedule.IsEnabled = True
+    End Sub
+
+    Private Sub CheckRapidStartCompatibility()
+        If Not IsHybridAvailiable() Then
+            togRapidStart.IsChecked = False
+            togRapidStart.IsEnabled = False
+            pnlUnsupported.Visibility = Visibility.Visible
+        End If
+    End Sub
 End Class
--- Countdown/Modules/Util.vb
+++ Countdown/Modules/Util.vb
@@ -8,4 +8,20 @@
         Return Environment.CurrentDirectory & "\Config\config.ini"
     End Function
 
+    Public Function IsHybridAvailiable() As Boolean
+        ' Check for windows version
+        ' Hybrid only availiable on Windows 8 or higher version
+        If Environment.OSVersion.Version.Major < 6 Then
+            Return False
+            Exit Function
+        End If
+
+        If Environment.OSVersion.Version.Minor < 2 Then
+            Return False
+            Exit Function
+        End If
+
+        Return True
+    End Function
+
 End Module
